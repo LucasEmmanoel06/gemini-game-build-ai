2574f20981759bea46fa0047a8e671a1561e76da
LucasEmmanoel06 <lucas.albuquerque2k@gmail.com>
2025-07-09 20:59:43 -0300

    docs: commented API key in script.js removed

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const markdownToHTML = (text) => {
   return converter.makeHtml(text)
 }
 
-// AIzaSyDLoveup9rqh0HBT5QPRiqfFM8K7ohU-c8
 const perguntarAI = async (prompt, game, apiKey) => {
   const model = "gemini-2.5-flash"
   const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
